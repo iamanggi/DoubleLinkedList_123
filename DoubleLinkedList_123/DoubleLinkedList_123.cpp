95e1f7cfbea7ae49cd8514cdd3a701ce16e9506c
iamanggi <anggipuspita452@gmail.com>
2023-05-25 16:04:02 +0700

    melakukan step 1 a pada methode search

--- DoubleLinkedList_123/DoubleLinkedList_123.cpp
+++ DoubleLinkedList_123/DoubleLinkedList_123.cpp
@@ -74,3 +74,7 @@ void DoubleLinkedList::addNode() {
 		current->next->prev = newNode;		//step 6
 	current->next = newNode;				//step 7
 }
+
+bool DoubleLinkedList::search(int rollNo, Node** previous, Node** current) {
+	*previous = START;				//step 1.a
+}
