fee20aaeb6b941259c7ef10b22160b5ba9d6175e
iamanggi <anggipuspita452@gmail.com>
2023-05-25 16:00:33 +0700

    membuat method add node pada void double link list

--- DoubleLinkedList_123/DoubleLinkedList_123.cpp
+++ DoubleLinkedList_123/DoubleLinkedList_123.cpp
@@ -28,3 +28,49 @@ public:
 DoubleLinkedList::DoubleLinkedList() {
 	START = NULL;
 }
+
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name of the student: ";
+	cin >> nm;
+	Node* newNode = new Node();						//step 1
+	newNode->noMhs = nim;							//step 2
+	newNode->name = nm;								//step 2
+
+	/*insert a node in the beginning of a doubly - linked list*/
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+
+		}
+		newNode->next = START;						//step 3
+		if (START != NULL)
+			START->prev = newNode;					//step 4
+		newNode->prev = NULL;						//step 5
+		START = newNode;							//step 6
+		return;
+	}
+	/*inserting a Node Between Two Nodes in the list*/
+	Node* current = START;							//step 1.a
+	Node* previous = NULL;							//step 1.b
+	while (current->next != NULL && current->next->noMhs < nim)			//step 1.c
+	{
+		previous = current; //1.d
+		current = current->next; //1.e
+	}
+
+	if (current->next != NULL && nim == current->next->noMhs) {
+		cout << "\nDuplicate roll numbers not allowed" << endl;
+		return;
+	}
+
+	newNode->next = current->next;		//step 4
+	newNode->prev = current;			//step 5
+	if (current->next != NULL)
+		current->next->prev = newNode;		//step 6
+	current->next = newNode;				//step 7
+}
