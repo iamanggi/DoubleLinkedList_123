80d144c38746c8a6597fb9873e90db531e027f02
iamanggi <anggipuspita452@gmail.com>
2023-05-25 15:30:31 +0700

    membuat class double link list

--- DoubleLinkedList_123/DoubleLinkedList_123.cpp
+++ DoubleLinkedList_123/DoubleLinkedList_123.cpp
@@ -9,3 +9,18 @@ public:
 	Node* next;
 	Node* prev;
 };
+
+class DoubleLinkedList {
+private:
+	Node* START;
+public:
+	DoubleLinkedList();
+	void addNode();
+	bool search(int rollNO, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void traverse();
+	void revtraverse();
+	void happus();
+	void searchData();
+};
